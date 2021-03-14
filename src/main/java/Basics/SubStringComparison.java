7776e090b9edefd8624890364e0de3bd471b4b5c
Arpad Nyircsak <69168570+Pyriel77@users.noreply.github.com>
2021-03-14 17:50:09 +0100

    SubString & SubStringComp is ok

---
+++ src/main/java/Basics/SubStringComparison.java
@@ -0,0 +1,35 @@
+package Basics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class SubStringComparison {
+
+    public static String getSmallestAndLargest(String s, int k) {
+
+        List<String> subs = new ArrayList<>();
+
+        if (s.length() == 0) {
+            return "Empty string";
+        }
+
+        if (s.length() < k) {
+            return s;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if(k + i > s.length()) {
+                break;
+            }
+            subs.add(s.substring(i, k+i));
+        }
+
+        Collections.sort(subs);
+        String smallest = subs.get(0);
+        String largest = subs.get(subs.size() - 1);
+
+
+        return smallest + "\n" + largest;
+    }
+}
