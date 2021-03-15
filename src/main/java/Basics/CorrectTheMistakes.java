19ced74422941ebb07b633f72e22760ea3bb451e
Arpad Nyircsak <69168570+Pyriel77@users.noreply.github.com>
2021-03-15 19:23:14 +0100

    CorrectTheMistakes is ok

---
+++ src/main/java/Basics/CorrectTheMistakes.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class CorrectTheMistakes {
+
+    public static String correct(String string) {
+
+        String corr = string.replace('5', 'S');
+        corr = corr.replace('0', 'O');
+        corr = corr.replace('1', 'I');
+
+        return corr;
+    }
+}
